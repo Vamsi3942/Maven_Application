375495c79a464f9453f2e620fc83afbc6869fc17
vamsi3942 <vamsiraju927@gmail.com>
2021-05-30 00:14:26 +0530

    first commit

---
+++ src/main/java/in/ashokit/Application.java
@@ -0,0 +1,20 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import in.ashokit.service.ReportService;
+
+@SpringBootApplication
+public class Application
+{
+
+	public static void main(String[] args) 
+	{
+		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+		ReportService rs = run.getBean(ReportService.class);
+		rs.generateReport();
+	}
+
+}
---
+++ src/main/java/in/ashokit/service/ReportService.java
@@ -0,0 +1,26 @@
+package in.ashokit.service;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.ashokit.Utils.DateUtils;
+
+@Service
+public class ReportService 
+{
+	public ReportService() 
+	{
+		System.out.println("ReportService:::Constructor");
+	}
+	@Autowired
+	private DateUtils dateUtils;
+	
+	public void generateReport()
+	{
+		LocalDate date = dateUtils.getDate();
+		System.out.println("Report Generated for::: " +date);
+	}
+}
+
